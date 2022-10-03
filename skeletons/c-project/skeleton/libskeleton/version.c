2502b53455ef6c8018fdd8b954f4ee82eb444a21
Tim Rose <tim.rose@acm.org>
2022-10-03 20:52:36 +1000

    feat: add version.c

---
+++ skeletons/c-project/skeleton/libskeleton/version.c
@@ -0,0 +1,10 @@
+/*
+ * VERSION.C --Version definition for this library.
+ */
+#include <xtd.h>
+
+#ifndef VERSION
+#define VERSION "unknown"
+#endif  /* VERSION */
+
+const char *name_version = VERSION;
