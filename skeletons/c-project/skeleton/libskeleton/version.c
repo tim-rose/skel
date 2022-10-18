ec73689a22b7943463c6fa7ebe92d10d8e257e10
Tim Rose <tim.rose@acm.org>
2022-10-18 16:35:31 +1100

    feat: rename version variable

--- skeletons/c-project/skeleton/libskeleton/version.c
+++ skeletons/c-project/skeleton/libskeleton/version.c
@@ -1,10 +1,9 @@
 /*
  * VERSION.C --Version definition for this library.
  */
-#include <xtd.h>
 
 #ifndef VERSION
 #define VERSION "unknown"
 #endif  /* VERSION */
 
-const char *name_version = VERSION;
+const char *skeleton_version = VERSION;
