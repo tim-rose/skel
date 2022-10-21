7848f5b99e2926e2465df06685c29cdb3bfd5c5b
Tim Rose <tim.rose@acm.org>
2022-10-21 14:43:15 +1100

    feat: removed skeletons to skel-templates

--- skeletons/c-project/skeleton/README.md
+++
@@ -1,1 +0,0 @@
-../../../bones/README.md
--- skeletons/c-project/skeleton/libskeleton/version.c
+++
@@ -1,9 +0,0 @@
-/*
- * VERSION.C --Version definition for this library.
- */
-
-#ifndef VERSION
-#define VERSION "unknown"
-#endif  /* VERSION */
-
-const char *skeleton_version = VERSION;
