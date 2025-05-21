9687f544d8dad8a3430e227aaf9c634fa93d1227
Tim Rose <tim.rose@acm.org>
2025-05-21 11:52:47 +1000

    feature: first attempt at directory unpacking tests

--- test/test-shar.sh
+++ test/test-shar.sh
@@ -2,7 +2,11 @@
 #
 # TEST-SHAR.SH --Unit tests for the shell archiver.
 #
-PATH=..:$PATH
+# Contents:
+# mk_shar()          --Make a shell archive from the specified files.
+# unpack_shar()      --Unpack a shell archive into a subdirectory.
+# test_unpack_file() --Test that a file is unpacked correctly from an archive.
+#
 . midden
 require tap
 require test-more
@@ -10,21 +14,30 @@ require test-more
 # atexit "rm *.sha *.txt"
 #atexit "rm -rf unpack-data"
 
+#
+# mk_shar() --Make a shell archive from the specified files.
+#
 mk_shar()
 (   # in a sub-shell
     cd data; echo "$@" | ../../$archdir/skel-shar -q
 )
 
+#
+# unpack_shar() --Unpack a shell archive into a subdirectory.
+#
 unpack_shar()
 {
     local dir="$1" shar="$PWD/$2"; shift 2
 
     rm -rf "$dir"
-    mkdir "$dir" 
+    mkdir "$dir"
     ( cd "$dir" && sh "$shar" "$@"; )
 }
 
-test_unpack()
+#
+# test_unpack_file() --Test that a file is unpacked correctly from an archive.
+#
+test_unpack_file()
 {
     local file="$1"
     local size=$(stat --format '%s' "data/$file")
@@ -32,18 +45,18 @@ test_unpack()
 
     mk_shar "$file" > data.sha
     unpack_shar unpack-data data.sha | diag
-    
-    test -e "unpack-data/$file"
+
+    test -f "unpack-data/$file"
     ok "$?" "unpack %s" "$file"
     ok_eq "$(stat --format '%s' unpack-data/$file)" "$size" "%s size (%d)" "$file" "$size"
     ok_eq "$(sum <unpack-data/$file)" "$sum" "%s sum (%s)" "$file" "$sum"
 
     unpack_shar unpack-data data.sha "$file"
-    test -e "unpack-data/$file"
+    test -f "unpack-data/$file"
     ok_eq "$?" "0" "unpack %s if requested" "$file"
 
     unpack_shar unpack-data data.sha some-other-file
-    test -e "unpack-data/$file"
+    test -f "unpack-data/$file"
     ok_eq "$?" "1" "don't unpack %s if not requested" "$file"
 
     local dir=$(dirname "$file")
@@ -53,14 +66,27 @@ test_unpack()
     fi
 }
 
+test_unpack_dir()
+{
+    local dir="$1"
+
+    mk_shar "$dir" > data.sha
+    unpack_shar unpack-data data.sha 2>&1 | diag
+
+    test -d "unpack-data/$dir"
+    ok_eq "$?" 1 "dir is not unpacked alone (%s)" "$dir"
+}
+
 main()
 {
-    plan 21
+    plan 23
 
-    test_unpack empty-file.txt
-    test_unpack simple.txt
-    test_unpack simple.bin
-    test_unpack subdir/simple.txt
+    test_unpack_file empty-file.txt
+    test_unpack_file simple.txt
+    test_unpack_file simple.bin
+    test_unpack_file subdir/simple.txt
+    test_unpack_dir subdir
+    test_unpack_dir empty-subdir
 }
 
 main "$@"
